c60e20b67c8a555006cb88c72176133be60dfbd4
@kushagra24 <krishna619kk@gmail.com>
2023-07-05 22:09:48 +0530

    Router,folder structure and tailwind setup done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Signup from "./pages/Signup";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='text-7xl text-green-900 font-poppins '>AESS Dashboard</h1>
+      <Routes>
+        <Route element={<Signup />} path='/' />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
